0d1ca209b6acfc0664c2a8e2a84212d249abd34c
Robert Ruetz <ruetz.robert@gmail.com>
2016-08-06 00:09:16 -0500

    Added methods to start gathering necessary information to get files for processing.

--- MuteBeginning/MuteBeginning.cs
+++ MuteBeginning/MuteBeginning.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using SoundForge;
+using System.Windows.Forms;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,80 @@ public class EntryPoint
 {
     public void Begin(IScriptableApp app)
     {
-        ISfDataWnd wnd = app.ActiveWindow;
-        long currentPos = wnd.Cursor;
-        long secToSamp = app.CurrentFile.SecondsToPosition(.350);
-        wnd.SetCursorAndScroll(currentPos - secToSamp, DataWndScrollTo.NoMove);
+        string workingDir = GetWorkingDirectory(app);
+        int threshold = GetThreshold(app);
+        string outputPath = string.Empty;
+        if (!CreateOutputDirectory(workingDir, out outputPath))
+        {
+            Environment.Exit(-1);
+        }
+
+        // TODO: Handle input and output extensions (file types)
+        // TODO: Loop through the files and do the thing
+        
+    }
+
+    public bool CreateOutputDirectory(string workingDir, out string outputPath)
+    {
+        outputPath = Path.Combine(workingDir, "output");
+        try
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+        catch(Exception ex)
+        {
+            MessageBox.Show(string.Format("Could not create output directory.\n{0}", ex.Message));
+            return false;
+        }
+        return true;
+    }
+
+    public int GetThreshold(IScriptableApp app)
+    {
+        int threshold = int.MaxValue;
+        bool haveThresh = false;
+        while (!haveThresh)
+        {
+            string thresh = string.Empty;
+            SfHelpers.WaitForInputString("Enter the threshold in negative dB (i.e. -35)", "-35");
+            if (!int.TryParse(thresh, out threshold))
+            {
+                MessageBox.Show("You did not enter a valid number.");
+            }
+            else if (threshold > 0)
+            {
+                MessageBox.Show("You entered a positive number.");
+            }
+            else
+            {
+                haveThresh = true;
+            }
+        }
+        return threshold;
+    }
+
+    public string GetWorkingDirectory(IScriptableApp app)
+    {
+        string workingDir = string.Empty;
+        bool haveWorkingDir = false;
+        while (!haveWorkingDir)
+        {
+            workingDir = SfHelpers.ChooseDirectory("Select the folder containing files to work on.", @"C:\");
+            if (string.IsNullOrEmpty(workingDir))
+            {
+                MessageBox.Show("You must choose a valid folder containing audio files.");
+            }
+            else if (!Directory.Exists(workingDir))
+            {
+                MessageBox.Show(string.Format("{0} does not exist. Try again.", workingDir);
+            }
+            else
+            {
+                haveWorkingDir = true;
+            }
+            //TODO: Possibly check to see if there are files and if they are audio files
+        }
+        return workingDir;
     }
 
     public void FromSoundForge(IScriptableApp app)
